7565df5b5d40a122fdccf571864d085d25d7c95d
ErwinWijnveld <59865357+ErwinWijnveld@users.noreply.github.com>
2023-08-25 11:23:19 +0200

    Update StickyHeader.tsx “in november”

--- src/components/StickyHeader.tsx
+++ src/components/StickyHeader.tsx
@@ -1,7 +1,7 @@
 const StickyHeader = () => {
 	return (
 		<div className="fixed top-0 left-1/2 z-[99999] -translate-x-1/2 whitespace-nowrap rounded-b-full bg-light-blue px-6 py-2 text-center text-xs font-semibold text-white md:px-10 md:text-sm">
-			Wij openen en November!
+			Wij openen in November!
 		</div>
 	);
 };
