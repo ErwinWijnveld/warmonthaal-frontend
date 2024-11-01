e034aee94708f747d7b16c9c4c06ed440ae94167
Erwin Wijnveld <erwinwijnveld@mbp-van-erwin.home>
2024-11-01 10:24:02 +0100

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import '@/assets/styles/globals.scss';
-import 'swiper/css';
-
 import PageTransition from '@/components/PageTransition';
+import { GoogleTagManager } from '@next/third-parties/google';
 import type { AppProps } from 'next/app';
+import 'swiper/css';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<PageTransition>
 			<Component {...pageProps} />
+			<GoogleTagManager gtmId="GTM-W7LWGDPQ" />
 		</PageTransition>
 	);
 }
